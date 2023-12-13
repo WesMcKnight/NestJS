31523ffb191c0e9ac352dc18e4ff24201069d6fd
Wes McKnight <wesleymcknight17@gmail.com>
2023-12-12 19:19:27 -0600

    path parameters with decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import {Controller, Get, Post, Put, Delete, Param} from "@nestjs/common"
+import { data, ReportType, } from "src/data";
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports(
-    @Param('type') type: string
-  ){
-    return []
+  getAllReports(@Param('type') type: string) {
+    const reportType = type === "income" ? ReportType.INCOME: ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
   
   @Get(':id')
--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,35 @@
+export enum ReportType {
+    INCOME = "income",
+    EXPENSE = "expense"
+}
+
 export const data: Data = {
-    report: []
+    report: [
+        {
+            id: "uuid1",
+            source: "Salary",
+             amount: 7500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid2",
+            source: "YouTube",
+             amount: 2500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid3",
+            source: "Food",
+             amount: 500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.EXPENSE
+        }
+    ]
 }
 
 interface Data {
@@ -11,11 +41,6 @@ interface Data {
         updated_at: Date;
         type: ReportType;
     }[]
-}
-
-enum ReportType {
-    INCOME = "income",
-    EXPENSE = "expense"
 }
 
 data.report.push({
